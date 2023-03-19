e668cecd9d3f17608271dcc0b4c6a4f3309bfff4
Mayank Patel <9051383+maknahar@users.noreply.github.com>
2023-03-19 23:51:12 +0530

    Handle click event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,18 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = ["Delhi", "Mumbai", "Lucknow", "Chennai", "Bangalore"];
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
 
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
         {items.length === 0 && <p>No Item Found</p>}
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
