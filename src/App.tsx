a9e408fb89b24207902e5f08c8581f5797e76f32
Mayank Patel <9051383+maknahar@users.noreply.github.com>
2023-03-20 01:04:04 +0530

    Added callbackable button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>Hello World</Alert>
+      <Button
+        text="First Button"
+        onButtonClick={(e) => console.log(e)}
+      ></Button>
     </div>
   );
 }
